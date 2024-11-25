85924c105be2ca901589c57a77acc71a3ff6be93
unknown <fernandaminga42@gmail.com>
2024-11-25 15:41:54 -0500

    actualizado

--- src/main/java/com/mycompany/estructurasdedatos/Estructurasdedatos.java
+++ src/main/java/com/mycompany/estructurasdedatos/Estructurasdedatos.java
@@ -12,5 +12,6 @@ public class Estructurasdedatos {
 
     public static void main(String[] args) {
         System.out.println("Bienvenidos estudiantes de estructuras de datos");
+        System.out.println("Estructura de datos");
     }
 }
