1bce7b57cca8110c063531a3edd613a703c8cf87
unknown <fernandaminga42@gmail.com>
2024-11-25 15:38:23 -0500

    Mi proyecto de estructura

---
+++ src/main/java/com/mycompany/estructurasdedatos/Estructurasdedatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estructurasdedatos;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class Estructurasdedatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
